37d7b9aacc8ddb37b25f653bfa69797a07e97b7b
himo77 <56316743+himo77@users.noreply.github.com>
2020-04-08 16:59:59 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Geography_Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which ocean is the largest?";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(38, 88);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 31);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(532, 88);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(171, 53);
+            this.btnCheckAnswer.TabIndex = 2;
+            this.btnCheckAnswer.Text = "Check";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheckAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geography_Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+        
+
+
+            
+            
+            int oceanCount = 5;
+            string oceanInfo = $"There are {oceanCount} oceans on earth.";
+            //"There are 5 oceans on earth."
+
+            string ocean = "Pacific Ocean";
+            double oceanSize = 63800000;
+            string oceanSizeUnits = "square miles";
+
+            string pacificInfo = $"The {ocean} is {oceanSize} {oceanSizeUnits}.";
+            // "The Pacific Ocean is 63,800,000 square miles."
+            
+            string answer = txtAnswer.Text;
+            answer = answer.ToLower();
+            
+            if (answer.Contains("pacific"))
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, the answer is the Pacific Ocean", "Result");
+            }
+        }
+    }
+}
